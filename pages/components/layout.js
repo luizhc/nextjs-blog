8fa726897495fd23778da9a2bdf03c2e169756af
Luiz Henrique <luizhenriquecorreia1997@gmail.com>
2021-02-21 00:09:57 -0300

    feat: dynamic routes

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../../styles/utils.module.css'
 import Link from 'next/link'
+import utilStyles from '../../styles/utils.module.css'
+import styles from './layout.module.css'
 
 const name = 'Luiz Henrique'
 export const siteTitle = 'Next.js Sample Website'
